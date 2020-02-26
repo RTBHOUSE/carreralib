827f7f4387b5fcd3df9a69010c9e2d0e152eda7d
flowenol <flovenol@gmail.com>
2020-02-26 16:58:50 +0100

    desktop files and startup script

---
+++ start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gnome-terminal --title="Race" -- python3.7 -m carreralib
---
+++ start_rank.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+gnome-terminal --title="Rank" -- python3.7 -m webapp 
+xdg-open http://localhost:5000
